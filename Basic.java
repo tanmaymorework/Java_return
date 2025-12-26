5e3155afddb70abfb88fefedc36503190b2a6e93
tanmaymorework <tanmaymore.work@gmail.com>
2025-12-27 02:55:02 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// syntax = 
+			//  datatype [] variable_name = new datatype[size]; 
+		
+		// Q Store 5 roll no 
+		
+//		int [] rollno1 = new int [5];
+		// or
+//		int [] rollno2 = { 21,23,43,5,5};
+		
+		
+		int [] ros; // Declaration of an Arrays : ros is getting defined in the stack 	
+		
+		ros = new int[5]; // Initalization : actually object is created in the heap area
+		
+		// Null can be only assign to non primitive not primitive 
+	}
+
+}
